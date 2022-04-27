7aa6450afb2d88e2ff42a1b7d1fc383bd3efa811
MercyMoon <45549722+SamDamDing@users.noreply.github.com>
2022-04-27 10:48:36 -0400

    Fixed some bugs and made it pretty

--- disbot.py
+++ disbot.py
@@ -1,29 +1,34 @@
-import discord
-from discord.ext import commands
 import random
 from TryFingersButHole import genPhrase
 from TryFingersButHole import FingerMessage
 from dicts import links
+import discord
+from discord.ext import commands
+from discord_slash import SlashCommand
+from discord_slash.model import ButtonStyle
+from discord_slash.utils.manage_components import create_select, create_select_option, create_actionrow, create_button, create_actionrow, wait_for_component
 
 client = discord.Client()
 fMsgHeader = "Finger Message Generator"
-fMsgFooter = "Made by MercyMoon"
-HAND = FingerMessage
+#https://github.com/SamDamDing/TryFingersButHole
+#https://tinyurl.com/FingerMessageGenerator
+#https://tinyurl.com/TFBHFMG
+fMsgFooter = "Source Code: https://tinyurl.com/TFBHFMG"
+fGestWidth = 100
+fGestHeight = 100
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-
-
 @client.event
 async def on_message(message):
+    HAND = FingerMessage
     if message.author == client.user:
         return
 
     if message.content.startswith('$fingers'):
         genPhrase("r","r","r","r","r","r","r")
-        #hand = FingerMessage
         fMsg = HAND.Message
         fGest = HAND.Gesture
         fType = HAND.Type
@@ -33,12 +38,12 @@ async def on_message(message):
         file = discord.File(links.get(HAND.Gesture), filename="image.png")
         embedVar.set_image(url="attachment://image.png")
         embedVar.set_footer(text = fMsgFooter)
-
+        #used for determining if we need a gesture image or not.
         if fType == 1 or fType == 3:
-            await message.channel.send(file=file, embed=embedVar)
+            embedVar.add_field(name="Gesture:", value=fGest)
+            msgctx = await message.channel.send(file=file, embed=embedVar )
         else:
-            await message.channel.send(embed=embedVar)
-
+            msgctx = await message.channel.send(embed=embedVar)
         print(fMsg)
 
 client.run('Your Token Here')
