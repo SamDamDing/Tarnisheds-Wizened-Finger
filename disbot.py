e74e84cd1736611c9f4539b5d2f907bf693c6cf7
MercyMoon <45549722+SamDamDing@users.noreply.github.com>
2022-04-28 02:40:29 -0400

    Added appraisal system

--- disbot.py
+++ disbot.py
@@ -1,12 +1,15 @@
-import random
-from TryFingersButHole import genPhrase
-from TryFingersButHole import FingerMessage
-from dicts import links
+import asyncio
 import discord
+import random
+from discord import Embed
 from discord.ext import commands
 from discord_slash import SlashCommand
 from discord_slash.model import ButtonStyle
-from discord_slash.utils.manage_components import create_select, create_select_option, create_actionrow, create_button, create_actionrow, wait_for_component
+from discord_slash.utils.manage_components import create_select, create_select_option, create_button, create_actionrow, wait_for_component
+from dicts import MessageImageLinks, links, TierThresholds
+from TryFingersButHole import genPhrase, FingerMessage
+from modules import thresholdcheck
+
 
 client = discord.Client()
 fMsgHeader = "Finger Message Generator"
@@ -16,6 +19,8 @@ fMsgHeader = "Finger Message Generator"
 fMsgFooter = "Source Code: https://tinyurl.com/TFBHFMG"
 fGestWidth = 100
 fGestHeight = 100
+emoji_list = ["👍", "👎"]
+appraisaltimeout = 30
 
 @client.event
 async def on_ready():
@@ -23,27 +28,62 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    HAND = FingerMessage
+    appraisalcount = 0
+    currenttier = thresholdcheck(appraisalcount)
+    
+    channel = message.channel
     if message.author == client.user:
         return
 
     if message.content.startswith('$fingers'):
         genPhrase("r","r","r","r","r","r","r")
+        HAND = FingerMessage
         fMsg = HAND.Message
         fGest = HAND.Gesture
         fType = HAND.Type
-        embedVar = discord.Embed(
-            title = fMsgHeader, description = HAND.Message, color = 0x2a2823,
-        )
-        file = discord.File(links.get(HAND.Gesture), filename="image.png")
-        embedVar.set_image(url="attachment://image.png")
+        
+        embedVar = discord.Embed(title = fMsgHeader, description = HAND.Message, color = 0x2a2823)
         embedVar.set_footer(text = fMsgFooter)
-        #used for determining if we need a gesture image or not.
+        embedVar.add_field(name="Appraisals: ", value=str(appraisalcount), inline=True)
+        
+        try:
+            file = discord.File(links.get(HAND.Gesture), filename="image.png")
+            embedVar.set_image(url="attachment://image.png")
+            embedVar.set_thumbnail(url=str(MessageImageLinks.get(currenttier)))
+        except:
+            pass
         if fType == 1 or fType == 3:
-            embedVar.add_field(name="Gesture:", value=fGest)
-            msgctx = await message.channel.send(file=file, embed=embedVar )
+            try:
+                embedVar.add_field(name="Gesture:", value=fGest)
+                msgctx = await message.channel.send(file=file, embed=embedVar)
+            except:
+                pass
         else:
             msgctx = await message.channel.send(embed=embedVar)
+        for i in emoji_list:
+            await msgctx.add_reaction(i)
+        await asyncio.sleep(5)
+        
+        message_1 = await msgctx.channel.fetch_message(msgctx.id)
+        tally = {x: 0 for x in emoji_list}
+        voters = []
+        for reaction in message_1.reactions:
+            if reaction.emoji in emoji_list:
+                reactors = await reaction.users().flatten()
+                for reactor in reactors:
+                    if reactor.id not in voters:
+                        if message_1.author != reactor:
+                            tally[reaction.emoji] += 1
+                            voters.append(reactor.id)
+                    await reaction.remove(reactor)
+        for x in voters:
+            appraisalcount =+ 1
+        currenttier = thresholdcheck(appraisalcount)
+        embedVar.set_thumbnail(url=str(MessageImageLinks.get(currenttier)))
+        embedVar.set_footer(text = fMsgFooter)
+        embedVar.set_field_at(index=0, name="Appraisals: ", value=str(appraisalcount), inline=True)
+        embed_1 = embedVar
+        await msgctx.edit(embed = embed_1)
         print(fMsg)
 
 client.run('Your Token Here')
