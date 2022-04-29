e8b69980475c3798e28c98b7630798085348238c
MercyMoon <45549722+SamDamDing@users.noreply.github.com>
2022-04-29 06:34:43 -0400

    Added use of arguments

--- disbot.py
+++ disbot.py
@@ -1,106 +1,159 @@
+import logging
+from genGesture import genGesture
 import asyncio
-import time
 import discord
-import random
-from discord import utils
-from discord import Embed
 from discord.ext import commands
-from discord_slash import SlashCommand
-from discord_slash.model import ButtonStyle
-from discord_slash.utils.manage_components import create_select, create_select_option, create_button, create_actionrow, wait_for_component
-from dicts import MessageImageLinks, links, TierThresholds
-from TryFingersButHole import genPhrase, FingerMessage
-from modules import thresholdcheck
+from dicts import Conjunctions, Gestures, MessageImageLinks, links, Categories
+from TryFingersButHole import genTemplate
+from modules import genMsgType, phraser, thresholdcheck
+logging.basicConfig(filename='log.txt', encoding='utf-8', level=logging.DEBUG)
 
-
-client = discord.Client()
-fMsgHeader = "Finger Message Generator"
-#https://github.com/SamDamDing/TryFingersButHole
-#https://tinyurl.com/FingerMessageGenerator
-#https://tinyurl.com/TFBHFMG
+intents = discord.Intents.all()
+guilds_ids = []
+fMsgHeader = "Message:"
 fMsgFooter = "Source Code: https://tinyurl.com/TFBHFMG"
 fGestWidth = 100
 fGestHeight = 100
 emoji_list = ["👍", "👎"]
-appraisaltimeout = 30
+goodemoji = "👍"
+bademoji = "👎"
+appraisaltimeout = 15
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+async def func():
+    for guild in bot.guilds:
+        id = guild.id
+        logging.debug(id) # As your BOT is in one single server, only one ID would be extracted and this can be used.
+        guilds_ids.append(id)
 
-mess = ""
-embedmess = ""
-HAND = FingerMessage
+bot = commands.Bot(command_prefix='$', intents=intents)
+bot.author_id = 968856972281675856
+@bot.event 
+async def on_ready():  # When the bot is ready
+    print("I'm in")
+    print(bot.user)
 
-@client.event
-async def on_message(message):
+@bot.command()
+async def fingers(message, *args):
+    global ctx
+    global MsgEmbed
+
+    msgtype =       "r"
+    msgtemplate =   "r"
+    msgword =       "r"
+    msgconjunction ="r"
+    msggesture =    "r"
+    msgtemplate2 =  "r"
+    msgword2 =      "r"
+
     appraisalcount = 0
     currenttier = thresholdcheck(appraisalcount)
     channel = message.channel
-    if message.author == client.user:
+    try:
+        argmsgtype = args[0]
+        if genMsgType(argmsgtype) != None:
+            msgtype = argmsgtype
+    except:
+        argmsgtype = genMsgType(msgtype)
+        msgtype = argmsgtype
+    try:
+        argmsgtemplate = args[1]
+        if genTemplate(argmsgtemplate) != None:
+            msgtemplate = argmsgtemplate
+    except:
+        msgtemplate = msgtemplate
+        pass
+    try:
+        argmsgword = args[2]
+        if any(argmsgword in val for val in Categories.values()) == True:
+            msgword = argmsgword
+    except:
+        msgword = msgword
+        pass
+    try:
+        argmsgconjunction = args[3]
+        if argmsgconjunction in Conjunctions:
+            msgconjunction = argmsgconjunction
+    except:
+        msgconjunction = msgconjunction
+        pass
+    try:
+        argmsgtemplate2 = args[4]
+        if genTemplate(argmsgtemplate2) != None:
+            msgtemplate2 = argmsgtemplate2
+    except:
+        pass
+    try:
+        argmsgword2 = args[5]
+        if any(argmsgword2 in val for val in Categories.values()) == True:
+            msgword2 = argmsgword2
+    except:
+        msgword2 = msgword2
+        pass
+    try:
+        argmsggesture = args[6]
+        if argmsggesture in Gestures:
+            msggesture = argmsggesture
+    except:
+        pass
+    if message.author == bot.user:
         return
-    if message.content.startswith('$fingers'):
-        genPhrase("r","r","r","r","r","r","r")
-        #HAND = FingerMessage
-        fMsg = HAND.Message
-        fGest = HAND.Gesture
-        fType = HAND.Type
-        embedVar = discord.Embed(title = fMsgHeader, description = HAND.Message, color = 0x2a2823)
-        embedVar.set_footer(text = fMsgFooter)
-        embedVar.add_field(name="Appraisals: ", value=str(appraisalcount), inline=True)
-        try:
-            file = discord.File(links.get(HAND.Gesture), filename="image.png")
-            embedVar.set_image(url="attachment://image.png")
-            embedVar.set_thumbnail(url=str(MessageImageLinks.get(currenttier)))
-        except:
-            pass
-        if fType == 1 or fType == 3:
-            try:
-                embedVar.add_field(name="Gesture:", value=fGest)
-                msgctx = await message.channel.send(file=file, embed=embedVar)
-            except:
-                pass
-        else:
-            msgctx = await message.channel.send(embed=embedVar)
-        for i in emoji_list:
-            await msgctx.add_reaction(i)
 
-    mess = msgctx
-    id = msgctx.id
-    await asyncio.sleep(5)
+    global fMsg
+    global fType
+    global fGest
+
+    fMsg = phraser(msgtype,msgtemplate,msgword,msgconjunction,msgtemplate2,msgword2)
+    fType = genMsgType(msgtype)
+    fGest = genGesture(msggesture)
+    
+    MsgEmbed = discord.Embed(title = fMsgHeader, description = fMsg, color = 0x2a2823)
+    MsgEmbed.set_footer(text = fMsgFooter)
+    MsgEmbed.add_field(name="Appraisals: ", value=str(appraisalcount), inline=True)
+    file = discord.File(links.get(fGest), filename="image.png")
+    MsgEmbed.set_image(url="attachment://image.png")
+    MsgEmbed.set_thumbnail(url=str(MessageImageLinks.get(currenttier)))
+
+    if fType == 1 or fType == 3:
+        MsgEmbed.add_field(name="Gesture:", value=fGest)
+        ctx = await message.channel.send(file=file, embed=MsgEmbed)
+    else:
+        ctx = await message.channel.send(embed=MsgEmbed)    
+    for i in emoji_list:
+        await ctx.add_reaction(i)
+    id = ctx.id
+    await asyncio.sleep(appraisaltimeout)
     message = await channel.fetch_message(id)
-    print(message.reactions)
+    logging.debug(message.reactions)
     voters = []
     for reaction in message.reactions:
-        if reaction.emoji == "👍":
+        if reaction.emoji == goodemoji:
             goodcount = reaction.count
             goodusers = [gooduser async for gooduser in reaction.users()]
-            #print("Goodcount: " + str(goodcount))
+            logging.debug("Goodcount: " + str(goodcount))
             for user in goodusers:
-                #print("Good Users: " + str(user.id))
+                logging.debug("Good Users: " + str(user.id))
                 if user.id not in voters:
                     voters.append(user.id)
                 await message.remove_reaction(reaction, user)
-        if reaction.emoji == "👎":
+        if reaction.emoji == bademoji:
             badcount = reaction.count
             badusers = [baduser async for baduser in reaction.users()]
-            #print("Badcount: " + str(badcount))
+            logging.debug("Badcount: " + str(badcount))
             for user in badusers:
-                #print("Bad Users: " + str(user.id))
+                logging.debug("Bad Users: " + str(user.id))
                 if user.id not in voters:
                     voters.append(user.id)
                 await message.remove_reaction(reaction, user)
-    #for voter in voters:
-    #    print("Voter: " + str(voter))
+    for voter in voters:
+        logging.debug("Voter: " + str(voter))
     appraisalcount = len(voters)-1
-    #print("Total Appraisals: " + str(appraisalcount))
-
+    logging.debug("Total Appraisals: " + str(appraisalcount))
     currenttier = thresholdcheck(appraisalcount)
-    embedVar.set_thumbnail(url=str(MessageImageLinks.get(currenttier)))
-    embedVar.set_footer(text = fMsgFooter)
-    embedVar.set_field_at(index=0, name="Appraisals: ", value=str(appraisalcount), inline=True)
-    embed_1 = embedVar
-    await mess.edit(embed = embed_1)
-    #print(fMsg)
+    MsgEmbed.set_thumbnail(url=str(MessageImageLinks.get(currenttier)))
+    MsgEmbed.set_footer(text = fMsgFooter)
+    MsgEmbed.set_field_at(index=0, name="Appraisals: ", value=str(appraisalcount), inline=True)
+    UpdatedMsgEmbed = MsgEmbed
+    await ctx.edit(embed = UpdatedMsgEmbed)
+    await bot.process_commands(message)
 
-client.run('Your Token Here')
+bot.run('Your Token Here')
